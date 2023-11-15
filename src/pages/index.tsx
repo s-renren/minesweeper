9065f1f98b75317f5161cf116cac906045e92157
s1F102301156 <s1F102301156@iniad.org>
2023-11-15 22:39:13 +0900

    基盤まで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import styles from './index.module.css';
 
 const Home = () => {
-  const number = 3;
+  const board = [...Array(9)].map((_, y) => [...Array(9)].map((_, x) => ((y + x + 1) %  13) - 1));
   return (
     <div className={styles.container}>
-      <div 
-        className={styles.number}
-        style={{ backgroundPositionX: 30 - 30 * number }} 
-      />
-
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((number, x) => (
+            <div
+              className={number === -1 ? styles.stone : styles.number}
+              style={{ backgroundPositionX: 30 - 30 * number }}
+              key={`${y}-${x}`}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 };
