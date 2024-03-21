27754320e32302687b3582442cb9f35ef1e977f4
s-renren <s-renren@users.noreply.github.com>
2024-03-21 09:05:14 +0000

    deploy: 5e733a5c15454af28bd02fbaf82bb52cc23894dc

---
+++ _next/static/LM5qJNgESZzpDYcoEwPth/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/352d1e6e97fd9532.css","static/chunks/pages/index-9a8e45fed8a026be.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
