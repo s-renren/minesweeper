4d5b086993e3e4daf058ddbea6abb210e0027f8f
s-renren <s-renren@users.noreply.github.com>
2024-03-23 14:07:02 +0000

    deploy: c4a41f7df229554e212d6abfa3403d88b54869e9

---
+++ _next/static/NCI5MFBqp7Uz9VwfWJ5Eo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/5452f45fa7741f4a.css","static/chunks/pages/index-19596e19f6892598.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/nYjN2TMpkOiaFkinRHyd7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/2e01096de1c8518f.css","static/chunks/pages/index-19596e19f6892598.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
